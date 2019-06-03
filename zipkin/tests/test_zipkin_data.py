eed5e2a55bdd1b28c40affd793c6a1599afd0c49
dimitar <dimitar@thoughtmachine.net>
2019-06-03 19:50:05 +0100

    add support for 128 bit Trace IDs
    The Trade ID has moved to 128 bits but our version and upstream don't
    support it which causes interop problems when parsing the hex headers

--- zipkin/tests/test_zipkin_data.py
+++ zipkin/tests/test_zipkin_data.py
@@ -1,5 +1,5 @@
 from unittest2.case import TestCase
-from zipkin.zipkin_data import ZipkinId
+from zipkin.zipkin_data import ZipkinId, ZipkinTraceId
 
 
 __all__ = ['ZipkinIdTestCase']
@@ -16,7 +16,7 @@ class ZipkinIdTestCase(TestCase):
         val = 42
         zid = ZipkinId(val)
         self.assertEqual(zid.get_binary(), val)
-        self.assertEqual(zid.get_hex(), '000000000000002a')
+        self.assertEqual(zid.get_hex(), b'000000000000002a')
 
     def test_from_binary(self):
         val = 2 * 63 - 1
@@ -31,8 +31,45 @@ class ZipkinIdTestCase(TestCase):
         for hex, expected_binary in cases:
             zid = ZipkinId.from_hex(hex)
             self.assertEqual(zid.get_binary(), expected_binary, hex)
-            self.assertEqual(zid.get_hex(), hex.zfill(16))
+            self.assertEqual(zid.get_hex(), hex.zfill(16).encode())
 
     def test_None_input(self):
         self.assertIsNone(ZipkinId.from_hex(None))
         self.assertIsNone(ZipkinId.from_binary(None))
+
+
+class ZipkinTraceIdTestCase(TestCase):
+    def test_raises_excepion_if_value_is_too_big(self):
+        with self.assertRaises(ValueError):
+            ZipkinTraceId(2 ** 63, 2)
+        with self.assertRaises(ValueError):
+            ZipkinTraceId(2, 2 ** 63)
+        with self.assertRaises(ValueError):
+            ZipkinTraceId(2, -(2 ** 63))
+        with self.assertRaises(ValueError):
+            ZipkinTraceId(-(2 ** 63), 2)
+
+    def test_getters(self):
+        low, high = 42, 24
+        zid = ZipkinTraceId(low=low, high=high)
+        self.assertEqual(zid.get_binary_low_bytes(), low)
+        self.assertEqual(zid.get_binary_high_bytes(), high)
+        self.assertEqual(zid.get_hex(), b'0000000000000018000000000000002a')
+
+    def test_from_hex(self):
+        cases = [
+            ('ffffffffffffffffffffffffffffffff', (-1, -1)),
+            ('c564d8606f4400', (0, 55561450905617408)),
+            ('aa', (0, 170)),
+            ('0000000000000018000000000000002a', (24, 42))
+        ]
+        for hex, (expected_high, expected_low) in cases:
+            zid = ZipkinTraceId.from_hex(hex)
+            self.assertEqual(zid.get_binary_high_bytes(), expected_high, hex)
+            self.assertEqual(zid.get_binary_low_bytes(), expected_low, hex)
+            self.assertEqual(zid.get_hex(), hex.zfill(32).encode())
+
+    def test_None_input(self):
+        self.assertIsNone(ZipkinTraceId.from_hex(None))
+        self.assertIsNone(ZipkinTraceId.from_binary(None, 123))
+        self.assertIsNone(ZipkinTraceId.from_binary(123, None))
